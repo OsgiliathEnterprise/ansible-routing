e442232141093cef6850a222ccf636da2a7d2c24
Charlie Mordant <cmordant1@gmail.com>
2020-04-27 13:37:05 +0200

    open port that we redirect

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,7 +2,7 @@
 
 
 def test_masquerade_enabled(host):
-    command = """sudo firewall-cmd --query-masquerade | \
+    command = """sudo firewall-cmd --zone=public --query-masquerade | \
     grep -c yes"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
@@ -12,5 +12,11 @@ def test_redirect_rule_set(host):
     command = """sudo firewall-cmd --query-rich-rule=\"rule family=\"ipv4\" \
     forward-port port=\"6752\" \
     protocol=\"tcp\" to-port=\"22\" to-addr=\"192.168.1.10\"\" | grep -c yes"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_port_is_opened(host):
+    command = """sudo firewall-cmd --zone=public --query-port=\"6752/tcp\" | grep -c yes"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
