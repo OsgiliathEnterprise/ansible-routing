e46dc2113570b7b74c7e7059ddc91b1071d0a0a4
Charlie Mordant <cmordant1@gmail.com>
2020-06-02 17:48:41 +0200

    travis test

--- molecule/noprop/tests/test_default.py
+++ molecule/noprop/tests/test_default.py
@@ -1,8 +1,8 @@
 """Role testing files using testinfra."""
 
 
-# def test_masquerade_enabled(host):
-#    command = """sudo firewall-cmd --zone=public --query-masquerade | \
-#    grep -c no"""
-#    cmd = host.run(command)
-#    assert '1' in cmd.stdout
+def test_masquerade_enabled(host):
+    command = """sudo firewall-cmd --zone=public | \
+    grep -c eth0"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
